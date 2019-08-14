2f56e60ec3d2ded34028dae4f7027f4e28380609
Unknown <kylehinckley@hotmail.com>
2019-08-14 12:27:24 -0600

    Phaser Contact Sparks

---
+++ Space Punks/Assets/Scripts/ParticleLauncher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLauncher : MonoBehaviour
+{
+    public ParticleSystem Phaser;
+    public ParticleSystem Sparks;
+
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Phaser.Emit(1);            
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Phaser.GetCollisionEvents(other, collisionEvents);
+
+        foreach (var t in collisionEvents)
+        {
+            EmitAtLocation(t);
+        }
+    }
+
+    void EmitAtLocation(ParticleCollisionEvent particleCollisionEvent)
+    {
+        Sparks.transform.position = particleCollisionEvent.intersection;
+        Sparks.transform.rotation = Quaternion.LookRotation(particleCollisionEvent.normal);
+        Sparks.Emit(1);
+    }
+}
